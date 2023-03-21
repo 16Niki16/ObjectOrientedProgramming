d900b5f1192723e9e408cee655c662ef1f85ebc5
Nikolay Petrov <nikipetrov03@abv.bg>
2023-03-21 16:16:05 +0200

    fifth week

---
+++ task1.cpp
@@ -0,0 +1,13 @@
+
+/*#include <iostream>
+#include<fstream>
+int main(){
+    std::ifstream if_stream;
+    if_stream.open("filename.txt");
+    if (!if_stream.is_open()){
+        throw std::exception("Not opened yet");
+    }
+    if_stream.seekg(0,std::ios::end);
+    std::cout << if_stream.tellg();
+    if_stream.close();
+}*/
